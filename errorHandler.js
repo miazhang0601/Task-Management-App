a8aea52c84a99ab740405734be059f5a8dd2b7be
Mia Zhang <162060452+miazhang0601@users.noreply.github.com>
2024-07-19 16:57:35 -0600

    Delete errorHandler.js

--- errorHandler.js
+++
@@ -1,8 +0,0 @@
-const logger = require('../utils/logger');
-
-const errorHandler = (err, req, res, next) => {
-    logger.error(`${err.message} - ${req.method} ${req.originalUrl}`);
-    res.status(500).json({ message: 'Internal Server Error' });
-};
-
-module.exports = errorHandler;
