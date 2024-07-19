a6c8d4158908cae684bb1a20f3befe18ce8cf813
Mia Zhang <162060452+miazhang0601@users.noreply.github.com>
2024-07-19 16:47:57 -0600

    routes

--- auth.js
+++ auth.js
@@ -1,17 +1,39 @@
+const express = require('express');
 const jwt = require('jsonwebtoken');
-const { findUserById } = require('../models/user');
+const bcrypt = require('bcryptjs');
+const { createUser, findUserByUsername } = require('../models/user');
+const logger = require('../utils/logger');
+
+const router = express.Router();
 const secretKey = 'your_secret_key'; // Use environment variable in real applications
 
-const authenticate = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1];
-    if (!token) return res.sendStatus(401);
+router.post('/register', async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        if (findUserByUsername(username)) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        const user = await createUser(username, password);
+        logger.info(`User registered: ${username}`);
+        res.status(201).json({ message: 'User created', userId: user.id });
+    } catch (err) {
+        next(err);
+    }
+});
 
-    jwt.verify(token, secretKey, (err, decoded) => {
-        if (err) return res.sendStatus(403);
-        req.user = findUserById(decoded.userId);
-        next();
-    });
-};
+router.post('/login', async (req, res, next) => {
+    try {
+        const { username, password } = req.body;
+        const user = findUserByUsername(username);
+        if (!user || !(await bcrypt.compare(password, user.password))) {
+            return res.status(401).json({ message: 'Invalid credentials' });
+        }
+        const token = jwt.sign({ userId: user.id }, secretKey, { expiresIn: '1h' });
+        logger.info(`User logged in: ${username}`);
+        res.json({ token });
+    } catch (err) {
+        next(err);
+    }
+});
 
-module.exports = authenticate;
+module.exports = router;
